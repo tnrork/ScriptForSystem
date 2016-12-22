9f0d9291ca8b76f42ba2e7a402a2633136c7402f
tnrork <taner.orak@ogr.dpu.edu.tr>
2016-12-22 12:41:24 +0300

    Useful Bashscripts on Linux Servers

---
+++ newuser.sh
@@ -0,0 +1,22 @@
+#Adding new users to a Linux system
+
+#This script allows the root user or admin to add new users to the system in an easier way by just typing the user name and password (The password is entered in an encrypted manner). The below script is taken from here.
+
+#!/bin/bash
+# Script to add a user to Linux system
+if [ $(id -u) -eq 0 ]; then
+    read -p "Enter username : " username
+    read -s -p "Enter password : " password
+    egrep "^$username" /etc/passwd >/dev/null
+    if [ $? -eq 0 ]; then
+        echo "$username exists!"
+        exit 1
+    else
+        pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+        useradd -m -p $pass $username
+        [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+    fi
+else
+    echo "Only root may add a user to the system"
+    exit 2
+fi
